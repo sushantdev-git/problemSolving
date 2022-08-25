15bc17c1870e5f55659ef5d6fa35f8221dbcfb3f
sushantdev-git <sushantkuamr6700@gmail.com>
2022-08-25 09:03:04 +0530

    deleted some folders

---
+++ codeforces/816/C.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//directives
+#define pb push_back
+#define int long long
+#define ss second
+#define ff first
+
+void solve(){
+    int n;
+    cin>>n;
+
+
+}
+
+
+
+
+int32_t main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t=1;
+    cin>>t;
+    while(t--) solve();
+
+    return 0;
+}
